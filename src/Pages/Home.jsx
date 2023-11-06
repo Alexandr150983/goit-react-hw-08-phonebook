f31aeda8a2b14fed3efaa889dddd09478798d097
Alexandr150983 <alexandr150983@gmail.com>
2023-11-07 00:36:07 +0200

    add styled HomePage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Box, Text, Heading } from '@chakra-ui/react';
+import { Box, Text, Heading, Link, useColorModeValue } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 
 const HomePage = () => {
+  const tealColor = useColorModeValue('teal.400', 'teal.200');
+
   return (
-    <Box p={4}>
-      <Heading as="h1" fontSize="2xl" mb={4}>
-        Contacts book welcome page{' '}
+    <Box p={4} textAlign="center">
+      <Heading as="h1" fontSize="4xl" mb={4}>
+        Contacts Book Welcome Page{' '}
         <span role="img" aria-label="Greeting icon">
           📞
         </span>
       </Heading>
-      <Text>
-        Welcome to our contacts book. This is the homepage of the application.
+      <Text fontSize="2xl">
+        Welcome to our Contacts Book. This is the homepage of the application.
+        Get{' '}
+        <Link
+          as={RouterLink}
+          to="/login"
+          color={tealColor}
+          fontWeight="bold"
+          fontSize="2xl"
+        >
+          logging
+        </Link>{' '}
+        by logging in or{' '}
+        <Link
+          as={RouterLink}
+          to="/register"
+          color={tealColor}
+          fontWeight="bold"
+          fontSize="2xl"
+        >
+          register
+        </Link>{' '}
+        a new account.
       </Text>
     </Box>
   );
